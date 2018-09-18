38ab9d7141e6abe20a61e3a18b174074fdb99a82
Andrew Basinski <abasinski@PC241627.mathstat.uidaho.edu>
2018-09-18 12:42:23 -0700

    new file:   NewProphVacc/.DS_Store 	new file:   NewProphVacc/Data/.DS_Store 	new file:   NewProphVacc/Data/analyzeR_Cont.r 	new file:   NewProphVacc/Data/analyzeR_Cont.r~ 	new file:   NewProphVacc/Data/analyzeR_Hist.r 	new file:   NewProphVacc/Data/analyzeR_Hist.r~ 	new file:   NewProphVacc/Data/analyzeR_IpMat.r 	new file:   NewProphVacc/Data/analyzeR_Line.r 	new file:   NewProphVacc/Sim_Gillespie.cpp 	new file:   NewProphVacc/Sim_Gillespie_Freq.cpp 	new file:   NewProphVacc/Tools/#mol.jv.r# 	new file:   NewProphVacc/Tools/.DS_Store 	new file:   NewProphVacc/Tools/Functions.r 	new file:   NewProphVacc/Tools/Functions.r~ 	new file:   NewProphVacc/checkR.r 	new file:   NewProphVacc/checkR.r~ 	new file:   NewProphVacc/checkR_Freq.r 	new file:   NewProphVacc/checkR_Freq.r~

---
+++ NewProphVacc/Data/analyzeR_IpMat.r
@@ -0,0 +1,111 @@
+CondNonExt = TRUE
+SimName = "DeerMice_Ha_2_PBShortLag_TMax1Yr"
+parmat = read.table(file = paste("ParMat_", SimName, sep=''), header = F)
+names(parmat) = c('Par','b0','d','Bp','Nv','tv','gamv','gamp','tb','T','IpInit', 'TPathInv')
+parmat$R0approx = with(parmat, Bp*(b0*tb)/(T*d*(d+gamp)))
+parmat$R0star = with(parmat, R0approx*(1-Nv/(b0*tb + Nv*exp(-d*(T-tv)))))
+NPars = nrow(parmat)
+NTrials = 1000
+
+TExtMatFile = paste('TExtMat_',SimName, sep = '')
+TExtMat = read.table(TExtMatFile, header = FALSE)
+IpMatFile = paste('IpMat_',SimName, sep='')
+IpMat = read.table(IpMatFile, header = FALSE)
+
+
+#####################################
+######## EXTINCTION PLOT C ##########
+#####################################
+##Contour plot. Y-axis: TPathInv
+##X-axis: tv; Z-axis: F_Ext?
+XValName = 'tv'
+XVals = unique(parmat[,XValName])
+YValName = 'TPathInv'
+YVals = unique(parmat[,YValName])
+FixValName1 = 'Bp'
+FixVals1 = unique(parmat[,FixValName1])#c(0.00001,0.00005,0.0001)
+FixValName2 = 'IpInit'
+FixVals2 = c(1,5,10)
+FixValName3 = 'Nv'
+FixVals3 = c(450)
+
+FigFold = paste(SimName,'_Fig',sep='')
+if(!dir.exists(FigFold)){dir.create(FigFold)}
+
+nXVals <- length(unique(parmat[,XValName]))
+nYVals <- length(unique(parmat[,YValName]))
+nFixVals1 <- length(FixVals1)
+nFixVals2 <- length(FixVals2)
+nFixVals3 <- length(FixVals3)
+
+TCrit <- 365*1 
+
+require(RColorBrewer)
+
+i = 0
+for(i1 in 1:nFixVals1){
+    for(i2 in 1:nFixVals2){
+    for(i3 in 1:nFixVals3){
+        ResMat = matrix(ncol = nYVals, nrow = nXVals)
+        for(Yi in 1:nYVals){
+            for(Xi in 1:nXVals){
+                XVal = XVals[Xi]
+                YVal = YVals[Yi]
+                F1 = FixVals1[i1]
+                F2 = FixVals2[i2]
+                F3 = FixVals3[i3]
+                wifix = parmat[,XValName]==XVal & parmat[,YValName]==YVal & parmat[,FixValName1]==F1 & 
+		      parmat[,FixValName2]==F2 & parmat[,FixValName3]==F3 
+		IpInitVal = parmat[wifix,'IpInit']
+		winotzero = which(IpMat[wifix,]!=0)
+if(CondNonExt){
+                ResMat[Xi,Yi] = mean(unlist(IpMat[wifix,winotzero]))/IpInitVal
+}else{
+                ResMat[Xi,Yi] = mean(unlist(IpMat[wifix,]))/IpInitVal
+}
+            }
+	}
+zmin = 0
+zmax = max(1,ceiling(max(ResMat)))
+breaks = seq(zmin,zmax,by = 0.01)
+nbreaks = length(breaks)
+initcols = c('blue', 'cadetblue1', 'orange', 'yellow')#brewer.pal(, "Blues")
+cols = colorRampPalette(initcols)(nbreaks-1)
+
+if(zmin < zmax){
+if(CondNonExt){
+            FileName = paste('Ip1YrCOND_',TCrit,FixValName1, F1, FixValName2, F2,FixValName3,F3,'.png',sep='')      
+}else{
+            FileName = paste('Ip1Yr_',TCrit,FixValName1, F1, FixValName2, F2,FixValName3,F3,'.png',sep='')      
+}
+            png(file = paste( FigFold, '/', FileName, sep=''), height = 4, width = 5, units = 'in', res = 400)
+            par(mai = c(1,1,0.25,0.25))
+	    layout(mat = matrix(c(1,2),ncol = 2), widths = c(1,0.2))
+	    
+	    image(x = XVals, y = YVals, z = ResMat, col = cols, breaks = breaks, 
+	    	    xlab = 'Time of Vaccination', ylab = 'Time of Pathogen Invasion', 
+		    xaxt = 'n', yaxt = 'n')
+            axislabs = seq(0,365, by = 60)
+            axislabs1 = YVals
+	    axis(side = 1, labels = T, at = axislabs)
+	    axis(side = 2, labels = axislabs, at = seq(5*365,6*365, by = 60))
+
+	    #Build legend bar
+	    Zmat = matrix(breaks, ncol = nbreaks)
+	    ZmatCent = matrix(0.5*(Zmat[-1] + Zmat[-length(Zmat)]), ncol = nbreaks-1)
+
+            par(mai = c(1,0.05,0.25,0.6))
+	    image(x = 1, y = Zmat, z = ZmatCent, col = cols, breaks = breaks, 
+	    	    ylim = range(breaks), xaxt = 'n', yaxt = 'n', xlab = '', ylab = '')
+	    axis(side = 4, las = 1)
+            dev.off()
+print(paste("Figure", i, "complete",sep = " " ))
+}else{
+print(paste("Figure", i, "fail",sep = " " ))
+}
+i=i+1
+    }}}#End Loop through fixed vals
+
+
+
+
---
+++ NewProphVacc/Sim_Gillespie.cpp
@@ -0,0 +1,410 @@
+
+/*
+Build a simple c++ script that will 
+simulate a stochastic SIR epidemiological
+model. The simulation procedes according
+to the Gillespie algorithm, and simulates 
+the use of vaccination to prevent the invasion
+of a zoonotic pathogen. 
+NewProphVacc runs the Gillespie algorithm 
+with the pathogen absent, 
+introduces a pathogen at time
+TPathInv, and begins vaccinating at time tv. 
+*/
+
+#include <iostream> // input/output std::cout, cin
+#include <math.h>   // Math functions
+#include <stdlib.h>
+#include <time.h>
+#include <fstream> //Used to open files to which data are saved
+#include <vector> 
+#include <string.h>
+#include <sys/stat.h> // mkdir
+#include <cmath> //fmod
+
+//---------------------------------------------------------START HEADER FILE
+
+//***********
+//CONSTANTS
+//***********
+const int NTrials = 1000;
+const int TPathLEN = 26;
+const int IpInitLEN = 1; int ipinitvals[]={10};
+const int tvLEN = 26; 
+const int BpLEN = 2; double bpvals[] = {0.00005, 0.00007};
+const int NvLEN = 2; double nvvals[] = {200.0,400.0};
+
+const int NParSets = 1352;
+
+const int NumPars = 12; //Number of columns in ParMat
+const bool VerboseWriteFlag = false;
+
+//********
+//USER-ASSIGNED VARIABLES
+char SimName[50] = "DeerMice_Base";
+
+std::vector<double> tvVals; double tvMIN = 8*365 + 0.1; double tvMAX = 9*365 - 0.1;
+std::vector<double> TPathInvVals; double TPathMIN = 8*365+0.01; double TPathMAX = 9*365 - 0.01; 
+std::vector<double> BpVals; 
+std::vector<double> NvVals;
+std::vector<int> IpInitVals; 
+
+//TMax is the time beyond pathogen introduction that should be simulated
+double TMax = 1.1*365.0; double tick = 1.0; //OneSim writes data at time-intervals tick
+
+int SInit = 1000;
+
+//********
+//ARRAYS
+//********
+double ParMat [NParSets][NumPars];
+double TExtMat [NParSets][NTrials];
+double IpMat [NParSets][NTrials];
+
+//*******************
+//CRITICAL VARIABLES
+//*******************
+int S, Iv, Ip, V, P, NPop, Par, IpInit;
+char FileNamePar[50];
+char FileNameTExt[50];
+char FileNameIpMat[50];
+char DirName[50] = "Data/";
+char FileSuffix[50], FileNameDat[50];
+double b0, tb, T, Nv,tv, b, gamv, R0p, Bp, gamp, d, Event_Rate, Event_Rate_Prod, RandDeath, dTime, t, ti, TPathInv;
+std::ofstream out_data;
+
+//OTHER VARIABLES
+int nbirths, ndeaths, ninfv, ninfp, nrecv, nrecp, S_death, Iv_death, Ip_death, V_death, P_death, svacc, npopvacc, totvacc, totbirthson, totbirthsoff;
+int ntrial, whichindex;
+double whichmin, unif, tmodT;
+double Nudge = 0.0000001;
+
+//FUNCTION DECLARATIONS
+void CheckEventConflict();
+void ApplyEvent();
+void GetTime();
+void Initialize(); //Fills ParMat, initializes TExtMat, returns #Rows
+void OneSim (double StartTime, double EndTime, bool StopOnErad);
+double Rand () ;
+std::vector<double> Seq(double, double, int); //Returns sequence
+void VaccFun();
+void WriteMat(double *arr, int NRow, int NCol, char* filename); 
+
+//---------------------------------------------------------END HEADER FILE
+
+//MAIN
+int main()
+{
+  srand ( time(NULL) );
+
+  //Build directory for simulation results
+  strcat(DirName, SimName);  
+  mkdir(DirName, ACCESSPERMS);
+  strcat(DirName, "/");	  
+
+  //Create filenames within directory for parmat, textmat, ipmat
+  strcpy(FileNamePar, DirName);  
+  strcat(FileNamePar, "ParMat");
+
+  strcpy(FileNameTExt, DirName);
+  strcat(FileNameTExt, "TExtMat");
+
+  strcpy(FileNameIpMat, DirName);
+  strcat(FileNameIpMat, "IpMat");
+
+  Initialize(); //Fill in the parameter matrix
+
+  WriteMat((double *)ParMat, NParSets, NumPars, FileNamePar); //Write ParMat
+
+  for(int Par = 0; Par < NParSets; Par++) //Loop through parameters
+    {
+      if(VerboseWriteFlag){
+	sprintf(FileSuffix, "Par_%d",Par);
+	strcpy(FileNameDat, DirName);
+	strcat(FileNameDat, FileSuffix);
+	out_data.open(FileNameDat);
+	out_data << "time S Iv Ip V P N births deaths ninfv ninfp nrecv nrecp S_death Iv_death Ip_death V_death P_death svacc npopvacc totvacc totbirthson totbirthsoff\n";
+      }
+      
+      //Extract parameters
+      //Parmat[0] corresponds to Par;
+      b0 = ParMat[Par][1]; //b0
+      d = ParMat[Par][2]; //d
+      Bp = ParMat[Par][3]; //Bp
+      //Nv set manually
+      tv = ParMat[Par][5]; //tv
+      gamv = ParMat[Par][6]; //gamv
+      gamp = ParMat[Par][7]; //gamp
+      tb = ParMat[Par][8]; //tb
+      T = ParMat[Par][9]; //T
+      IpInit = (int) ParMat[Par][10]; // Initial level of pathogen upon invasion
+      TPathInv = ParMat[Par][11]; // Time of pathogen invasion
+      
+      for(ntrial = 0; ntrial < NTrials; ntrial++)
+	{
+	  S = SInit; //S
+	  Iv = 0; //Iv
+	  Ip = 0; //Ip
+	  V = 0; //V
+	  P = 0; //P
+	  NPop = S + Iv + Ip + V + P;
+
+	  if(TPathInv < tv)
+	    {
+	      //Simulate to quasi steady state (rewrites State)
+	      Nv = 0.0;
+	      OneSim(0.0, TPathInv, false);
+	      
+	      //Simulate invasion until TMax years, or pathogen extinction
+	      Ip = IpInit; NPop +=IpInit;
+	      OneSim(TPathInv,tv, false);
+	      
+	      //Simulate vaccination for 1 year beyond TPathInv
+	      Nv = ParMat[Par][4]; //set Nv
+	      VaccFun(); //Vaccinate once
+	      Nv = 0.0; 
+		
+	      //Continue sim for TMax
+	      OneSim(tv, TPathInv+TMax, true);
+	    }else{
+	    //Simulate to quasi steady state (rewrites State)
+	      Nv = 0.0;
+	      OneSim(0.0, tv, false);
+
+	      //Simulate vaccination 
+	      Nv = ParMat[Par][4]; //set Nv
+	      VaccFun(); //Vaccinate once
+	      Nv = 0.0; 
+	      OneSim(tv, TPathInv, false);
+
+	      //Simulate invasion until TMax years, or pathogen extinction
+	      Ip = IpInit; NPop +=IpInit;
+	      OneSim(TPathInv,TPathInv+TMax, true);
+	  }
+
+	  //Store final value of t in TExtMat,
+	  //and final Ip value in IpMat.
+	  TExtMat[Par][ntrial] = t;
+	  IpMat[Par][ntrial] = (double) Ip;    
+	}//End loop through NTrials
+
+      if(VerboseWriteFlag)
+	{
+	  out_data.close();
+	}
+      
+      std::cout << "*****************************" << std::endl;	  
+      std::cout << "Finished Parameter Set " << Par+1 << " / " << NParSets << std::endl;
+      std::cout << "*****************************" << std::endl;
+      
+    }//End Loop through NParSets
+
+  WriteMat((double *)TExtMat, NParSets, NTrials, FileNameTExt); //Write TExtMat      
+  WriteMat((double *)IpMat, NParSets, NTrials, FileNameIpMat); //Write IpMat
+}//End Main
+
+      
+      
+//////////////////////////////////////
+///////// DEFINE FUNCTIONS ///////////
+//////////////////////////////////////
+
+//************************************
+//Function that updates state variables
+void ApplyEvent() {
+  if(Event_Rate_Prod <= b) //Birth
+    { S++; NPop++; nbirths++; } 
+  else if(Event_Rate_Prod <= b + d*NPop) //Death
+    {
+      RandDeath = Rand()*NPop;
+      if( RandDeath < S ) //S dies
+	{S--; S_death++;}
+      else if(RandDeath < S + Iv) //Iv dies
+	{Iv--; Iv_death++;}
+      else if(RandDeath < S + Iv + Ip) //Ip dies
+	{Ip--; Ip_death++;}
+      else if(RandDeath < S + Iv + Ip + V) //V dies
+	{V--; V_death++;}
+      else{P--; P_death++;}  //P dies	
+      NPop--; ndeaths++;
+    }  
+  else if(Event_Rate_Prod <= b + d*NPop + Bp*Ip*S)    //Event: Pathogen infection of S
+    {S--; Ip++; ninfp++;}
+  else if(Event_Rate_Prod <= b + d*NPop + Bp*Ip*S + Bp*Ip*Iv) //Event: Pathogen infection of V
+    {Iv--; Ip++; ninfp++;}
+  else if(Event_Rate_Prod <= b + d*NPop + Bp*Ip*S + Bp*Ip*Iv + gamv*Iv) //Event: Iv Recovery
+    {Iv--;V++;nrecv++;}
+  else if(Event_Rate_Prod <= b + d*NPop + Bp*Ip*S + Bp*Ip*Iv + gamv*Iv + gamp*Ip) //Event: Ip Recovery
+    {Ip--; P++; nrecp++;}
+}
+
+//*************************************
+//function that finds the min of 3 doubles, and min's index CHECK
+void CheckEventConflict (){
+  whichmin = T - tmodT;
+  whichindex = 0;
+  if(tmodT < tb && tb-tmodT < whichmin)
+    {
+      whichmin = tb-tmodT;
+      whichindex = 1;
+    }
+  if(tmodT < tv && tv-tmodT < whichmin)
+    {
+      whichmin = tv-tmodT;
+      whichindex = 2;
+    }
+}
+
+//************************************
+//Function that returns the time for the next event
+void GetTime (){
+  // Select random time step (doesn't allow for infite time step)
+  do
+    {
+      dTime = (double) rand() / RAND_MAX ; //Use var dTime for storage
+      dTime = -log(dTime) / Event_Rate; //Assign next timestep
+    } while (dTime == INFINITY); //Avoid infinite timesteps
+}
+
+//************************************
+//function to Initialize values of 2D array
+void Initialize()
+{
+  //tvVals.assign(tvvals, tvvals + tvLEN);
+  tvVals = Seq(tvMIN, tvMAX, tvLEN);
+  TPathInvVals = Seq(TPathMIN, TPathMAX, TPathLEN);
+  BpVals.assign(bpvals, bpvals + BpLEN);
+  IpInitVals.assign(ipinitvals, ipinitvals + IpInitLEN);
+  NvVals.assign(nvvals, nvvals + NvLEN);
+
+  //Fill in ParMat
+  int i = 0;
+  for(int i1=0; i1<tvVals.size(); i1++)
+    for(int i2=0; i2<BpVals.size(); i2++)
+      for(int i3=0; i3<TPathInvVals.size(); i3++)
+	for(int i4=0; i4<IpInitVals.size(); i4++)
+	  for(int i5=0; i5<NvVals.size(); i5++)
+	  {
+	    ParMat[i][0] = i; //Par
+	    ParMat[i][1] = 4.0;   //b0
+	    ParMat[i][2] = 0.004; //d
+	    ParMat[i][3] = BpVals[i2]; //Bp
+	    ParMat[i][4] = NvVals[i5]; //Nv
+	    ParMat[i][5] = tvVals[i1]; //tv
+	    ParMat[i][6] = 0.07; //gamv
+	    ParMat[i][7] = 0.007; //gamp
+	    ParMat[i][8] = 90.0; //tb
+	    ParMat[i][9] = 365.0; //T
+	    ParMat[i][10] = (double) IpInitVals[i4]; //IpInit
+	    ParMat[i][11] = TPathInvVals[i3]; //TPathInv
+	    i++;
+	  }
+}
+
+void OneSim (double StartTime, double EndTime, bool StopOnErad = false)
+{
+  //Set initial conditions: No Pathogen, no vaccination
+  ti = StartTime;
+  t = StartTime;
+
+  tmodT = std::fmod(t,T);
+  b = (double) b0*(tmodT < tb);
+
+  //Track births, deaths, recoveries, etc for error checking
+  nbirths = 0; ndeaths = 0; ninfv = 0; ninfp = 0; nrecv = 0; 
+  nrecv = 0; S_death = 0; Iv_death = 0; Ip_death = 0;  
+  V_death = 0; P_death = 0; svacc = 0; npopvacc = 0; 
+  totvacc = 0; totbirthson = 0; totbirthsoff = 0;
+
+  while(t < EndTime && (Ip > 0 || !StopOnErad))
+    {
+      if( t >= ti && VerboseWriteFlag) //Write old values at intervals "tick"
+	{
+	  out_data << t << " " << S << " " << Iv << " " << Ip << " " << V << " " << P  << " " << NPop << " " << nbirths << " " << ndeaths <<  " " << ninfv << " " << ninfp << " " << nrecv << " " << nrecp << " " << S_death << " " << Iv_death << " " << Ip_death << " " << V_death << " " << P_death << " " << svacc << " " << npopvacc << " " << totvacc << " " << totbirthson << " " <<  totbirthsoff << "\n"; 
+	  ti += tick;
+	  nbirths = 0; ndeaths = 0; svacc = 0; npopvacc = 0; totvacc = 0; totbirthson = 0; 
+	}  
+      Event_Rate = b + d*NPop + Bp*S*Ip + Bp*Iv*Ip + gamv*Iv + gamp*Ip;
+      Event_Rate_Prod = Event_Rate*Rand();
+      
+      GetTime(); //Get time to next event
+      
+      CheckEventConflict(); //Finds whichmin, the time of the next conflict
+
+      if(dTime < whichmin) //If no conflicts, proceed with Gillepsie event
+	{
+	  ApplyEvent();	
+	}
+      else{ //If conflict, stop at conflict and perform necessary action
+	
+	switch(whichindex)
+	  {
+	  case 0 : b = b0; totbirthson++; break; //Start of birthing season
+	  case 1 : b = 0.0; totbirthsoff++; break; //End of birthing season
+	  case 2 : VaccFun(); totvacc++; //Update S,Iv due to vaccination 
+	  }
+	dTime = whichmin + Nudge; //Nudge ensures that t moves past conflict
+      }//End else
+
+      t += dTime;
+      tmodT = std::fmod(t,T);
+
+    }//End While
+  
+  if(VerboseWriteFlag)
+    {
+      out_data << t << " " << S << " " << Iv << " " << Ip << " " << V << " " << P  << " " << NPop << " " << nbirths << " " << ndeaths <<  " " << ninfv << " " << ninfp << " " << nrecv << " " << nrecp << " " << S_death << " " << Iv_death << " " << Ip_death << " " << V_death << " " << P_death << " " << svacc << " " << npopvacc << " " << totvacc << " " << totbirthson << " " << totbirthsoff << "\n"; 
+    }
+}
+
+//************************************
+//Function returning a random number between 0 and 1
+double Rand (){
+  do
+    {
+      unif = (double) rand() / RAND_MAX ;
+    } while(unif==0.0);
+  return unif;
+}  
+
+//************************************
+//function that returns a sequence from minval to maxval, and of length lengthval
+std::vector<double> Seq(double minval, double maxval, int lengthval) {
+  std::vector<double> vec(lengthval);
+  if(lengthval==1)
+    {
+      vec[0] = minval;
+    }else{
+  for(int seqindex = 0; seqindex < lengthval; seqindex++)
+    {
+      vec[seqindex] = minval + (double) seqindex/(lengthval-1)*(maxval-minval);
+    }
+  }
+  return vec;
+}
+
+//************************************
+//Vaccination function
+void VaccFun(){
+  int temp = (int) round( (double) Nv*S/(std::max(NPop,1)));
+  temp  = std::min( temp,  S);
+  S -= temp;
+  Iv += temp;
+}
+
+//************************************
+//function to write values of 2D array
+void WriteMat(double *arr, int NRow, int NCol, char*filename)
+{
+  out_data.open(filename);
+  int i,j;
+  for(i=0; i<NRow;i++)
+    {
+      for(j=0; j<NCol; j++)
+	{
+	  out_data << *((arr+i*NCol) + j) << " "; 
+	}
+      out_data << std::endl;
+    }
+  out_data.close();
+}
