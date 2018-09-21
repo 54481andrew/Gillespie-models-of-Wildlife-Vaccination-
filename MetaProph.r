f35097aa995baf6706a06d60b2de4081d5f978ed
Andrew Basinski <abasinski@PC241627.mathstat.uidaho.edu>
2018-09-21 13:12:39 -0700

    modified:   .DS_Store 	modified:   4x4pane.tex 	modified:   MetaProph.r

--- MetaProph.r
+++ MetaProph.r
@@ -1,8 +1,8 @@
-BuildDataFlag = TRUE
-FoldName = "MetaProph_Fig"
+BuildDataFlag = FALSE
+FoldName = "MetaProphFreq_Fig"
 SimType = "ProphVacc"
-SimNameList = paste(SimType,"/Data/DeerMice_Base_varNv_redo",sep='')
-#SimNameList = paste(SimType,"/Data/DeerMice_Base_Freq_varNv",sep='')
+#SimNameList = paste(SimType,"/Data/DeerMice_Base_varNv_redo",sep='')
+SimNameList = paste(SimType,"/Data/DeerMice_Base_Freq_varNv",sep='')
 
 #Collect all data
 #for(i in 1:length(SimNameList)){
@@ -30,7 +30,7 @@ XVals = unique(parmat[,XValName])
 YValName = 'TPathInv'
 YVals = unique(parmat[,YValName])
 FixValName1 = 'Bp'
-FixVals1 = unique(parmat[,FixValName1])[1]  #c(0.00001,0.00005,0.0001)
+FixVals1 = unique(parmat[,FixValName1])[1:2]  #c(0.00001,0.00005,0.0001)
 FixValName2 = 'IpInit'
 FixVals2 = 10#unique(parmat[,FixValName2])
 FixValName3 = 'Nv'
@@ -103,6 +103,12 @@ nbreaks = length(FixVals3)
 initcols = c('purple', 'orange')#brewer.pal(, "Blues")
 cols = c('black','green','red')#colorRampPalette(initcols)(nbreaks-1)
 
+for(i1 in 1:nFixVals1){
+    for(i2 in 1:nFixVals2){
+	  	   
+    	   F1 = FixVals1[i1]
+	   F2 = FixVals2[i2]
+    	   wipar = MetaDat[,1]== F1 & MetaDat[,2]==F2
 
             FileName = paste('TPathAvg_',SimType, '_',FixValName1, F1, FixValName2, F2,'.png',sep='')      
             png(file = paste( FigFold, '/', FileName, sep=''), height = 4, width = 5, units = 'in', res = 400)
@@ -110,7 +116,7 @@ cols = c('black','green','red')#colorRampPalette(initcols)(nbreaks-1)
 	    
 	    if(SimType=="ProphVacc"){xrange <- c(0,365)}else{xrange <- c(8*365, 9*365)}
 
-	    matplot(matrix(MetaDat[1,-(1:3)], ncol=1),t(MetaDat[-1,-(1:3)]), xlim = xrange, ylim = c(0,1), xaxt = 'n', yaxt = 'n', 
+	    matplot(matrix(MetaDat[1,-(1:3)], ncol=1),t(MetaDat[wipar,-(1:3)]), xlim = xrange, ylim = c(0,1), xaxt = 'n', yaxt = 'n', 
 	    	     xlab = '', ylab = '', pch = 1, lwd = 2, cex = 1, col = cols, main = SimType)
 
 #	    lines(x = XVals%%365, y = TPathAvg, col = cols[i3], lwd = 2)
@@ -132,3 +138,4 @@ cols = c('black','green','red')#colorRampPalette(initcols)(nbreaks-1)
 	    legend(x = 'bottomright', legend = legendtext, pch = 1,pt.lwd = 2, col = cols)
             dev.off()
 
+}}
