6241121d3136c966393048e34cfee4deae535abb
Andrew Basinski <abasinski@PC241627.mathstat.uidaho.edu>
2018-10-09 21:05:19 -0700

    Added ODESimmR_optim.r script for simulating ODE eradication 	modified:   .DS_Store 	modified:   EradVacc/.DS_Store 	modified:   EradVacc/Data/.DS_Store 	modified:   EradVaccOnePulse/.DS_Store 	modified:   EradVaccOnePulse/Data/.DS_Store 	modified:   ProphVacc/.DS_Store 	modified:   ProphVacc/ODESimmR_optim.r

--- ProphVacc/ODESimmR_optim.r
+++ ProphVacc/ODESimmR_optim.r
@@ -1,8 +1,12 @@
-SimName = "LagImport"
+require(parallel)
 
-parmat = expand.grid(NPar = 0, b0 = NA, d = 0.002, rho = seq(0.01,1,length.out = 5),
-                     gamv =0.07, tb = seq(15,365,length.out = 5),
-                     T = 365, NAvg = 10000, tvstar = 0, vavgstar = 0, vnull = 0)
+SimName = "LagImport.TMax40.drates"
+
+parmat = expand.grid(NPar = 0, b0 = NA, d = c(0.0005,0.001,0.002,0.005), 
+       	             rho = seq(0.01,1,length.out = 50),
+                     gamv =0.07, tb = seq(15,365,length.out = 50),
+                     T = 365, NAvg = 10000, tvstar = 0, 
+		     vavgstar = 0, vnull = 0)
 
 NPars = nrow(parmat)
 parmat$NPar = 1:NPars
@@ -12,13 +16,18 @@ parmat$Nv = with(parmat,rho*NAvg)
 require(deSolve)
 source('Tools/Functions.r', local = TRUE)
 
-maxtimes <- 365*20
-times    <- seq(0,maxtimes, by = 0.01)
+nyears   <- 40
+maxtimes <- 365*nyears
+times    <- seq(0,maxtimes, by = 1)
+witimes <- times >= (nyears-1)*365
+yeartimes <- c(times[witimes]%%365)
+yeartimes[length(yeartimes)] <- 365
+tvvals <- matrix(seq(0.1,364.9,length.out = 25), ncol = 1)
 
 filename = paste("ODEData/", SimName, sep = '')
 write.table(parmat[1,][-1,], file = filename, append = F)
 
-tvvals <- matrix(seq(0.1,364.9,length.out = 20), ncol = 1)
+
 
 mcl.fun = function(i){
 
@@ -30,53 +39,44 @@ mcl.fun = function(i){
         NVacc <- parms$Nv
         vacctimes <- seq(tv, maxtimes,by = TVal)        
 
-        out  <- data.frame(lsoda(y = y0, times = seq(0, maxtimes, by=1), 
+        out  <- data.frame(lsoda(y = y0, times = times, 
                                  func = rhs.freq.jv.fun, parms = parms,
                                  events=list(func = vaccinate.jv, time=vacctimes),
                                  maxsteps = 100000))
         
 ###Return average serorpevalence
-        VAvg <- mean(with(out, V/(S + Iv + V)))
+	vals    <- with(out[witimes,], V/(S + Iv + V))
+	vals[1] <- vals[length(vals)]
+	vapprox = splinefun(x = yeartimes, vals, method = "periodic")
+        VAvg <- 1/365*(integrate(vapprox, lower = 0, upper = 365, stop.on.error = FALSE)$value)
         return(VAvg)
     }###End cost.fxn
 
     vnullvals <- apply(X = tvvals, MARGIN = 1, FUN = cost.fxn, parms = parmat[i,])
     tvstar <- tvvals[which.max(vnullvals)]
-    uroot <- optimize(f=cost.fxn, interval = c(0.67*tvstar, min(1.33*tvstar,365)), parmat[i,], maximum=TVal)
+
+    wi.interval <- c(max(1, which.max(vnullvals) - 1), min(which.max(vnullvals) + 1, length(tvvals)))
+    interval <- tvvals[wi.interval]
+
+    uroot <- optimize(f=cost.fxn, interval = interval, parmat[i,], maximum=TRUE)
     tvstar <- uroot$maximum ###Location
     vavgstar <- uroot$objective ###Maximum value
 
     parmat$tvstar[i] <- tvstar
     parmat$vavgstar[i] <- vavgstar
-    parmat$vnull[i]  <- mean(vnullvals)
+
+    vnullfun = splinefun(x = c(tvvals,365.1), c(vnullvals,vnullvals[1]))
+    parmat$vnull[i]  <- 1/365*integrate(vnullfun, lower = 0, upper = 365)$value
+
     write.table(parmat[i,], file = filename, append = T, col.names = F, row.names = F)
     print(paste("NPar:", i, ' / ', NPars))
     return(i)
 }
 
-metaout <- mclapply(X = 1:NPars, FUN = mcl.fun)
+metaout <- mclapply(X = 1:NPars, FUN = mcl.fun, mc.cores = detectCores()-2)
 
 refparmat <- read.table(file = filename, header = T)
 refparmat <- refparmat[order(refparmat$NPar),]
 write.table(refparmat, file = filename, append = F, row.names = F)
 parmat <- read.table(file = filename, header = T)
 
-XName <- 'tb'
-YName <- 'rho'
-XVals  <- unique(parmat[,XName])
-YVals  <- unique(parmat[,YName])
-nXVals <- length(XVals)
-nYVals <- length(YVals)
-
-Import.Mat = matrix(NA,nrow = nXVals, ncol = nYVals)
-Lag.Mat = matrix(NA,nrow = nXVals, ncol = nYVals)
-for(xi in 1:nXVals){
-    for(yi in 1:nYVals){
-        wi <- XVals[xi]==parmat[,XName] & YVals[yi]==parmat[,YName]
-        TvStar <- parmat$tvstar[wi]
-        VAvgStar <- parmat$vavgstar[wi]
-        VNull <-  parmat$vnull[wi]
-        Lag.Mat[xi,yi] <- TvStar - unique(parmat$tb[wi])
-        Import.Mat[xi,yi] <- VAvgStar/VNull
-    }
-}
